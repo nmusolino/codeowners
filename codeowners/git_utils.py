b932d99dc4f33e71514bf5d188fc95c637b163ad
Nicholas Musolino <n.musolino@gmail.com>
2018-10-07 20:17:44 -0400

    Add git_utils module

---
+++ codeowners/git_utils.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+# Possible locations of CODEOWNERS file, relative to repository root.
+_CODEOWNERS_REL_LOCATIONS = [Path('CODEOWNERS'), Path('docs/CODEOWNERS'), Path('.github/CODEOWNERS')]
+
+
+def git_repository_root(base_dir: Path, search_parent_directories=True) -> Path:
+    dir = base_dir
+    while dir != Path():
+        if (dir / '.git').exists():
+            return dir
+
+        if not search_parent_directories:
+            break
+
+        dir = dir.parent
+
+    raise FileNotFoundError("Could not find .git directory in:  {base_dir}{msg}".format(
+        base_dir=base_dir, msg=' (or any parent)' if search_parent_directories else ''))
+
+
+def codeowners_path(base_dir: Path) -> Path:
+    repo_root = git_repository_root(base_dir=base_dir)
+    candidate_paths = [repo_root / location for location in _CODEOWNERS_REL_LOCATIONS]
+    path = next((p for p in candidate_paths if p.exists()), None)
+    if path is None:
+        raise FileNotFoundError("Could not find CODEOWNERS file in any of the following locations: ".format(
+            '; '.join(candidate_paths)))
+    return path
---
+++ tests/test_git_utils.py
@@ -0,0 +1,33 @@
+import pathlib
+
+import pytest
+
+from codeowners import git_utils
+
+
+def test_git_repository_root():
+    this_file = pathlib.Path(__file__)
+    assert git_utils.git_repository_root(this_file) == this_file.parent.parent
+
+
+def test_git_repository_root_no_parents():
+    with pytest.raises(FileNotFoundError):
+        git_utils.git_repository_root(pathlib.Path(__file__), search_parent_directories=False)
+
+
+def test_git_repository_root_outside_repository():
+    with pytest.raises(FileNotFoundError):
+        git_utils.git_repository_root(pathlib.Path())
+        git_utils.git_repository_root(pathlib.Path('/'))
+
+
+def test_codeowners_path():
+    this_file = pathlib.Path(__file__)
+    expected_path = this_file.parent.parent / 'CODEOWNERS'
+    assert expected_path.exists()
+    assert git_utils.codeowners_path(this_file) == expected_path
+    assert git_utils.codeowners_path(this_file.parent) == expected_path
+    assert git_utils.codeowners_path(this_file.parent.parent) == expected_path
+
+    with pytest.raises(FileNotFoundError):
+        git_utils.codeowners_path(pathlib.Path())
