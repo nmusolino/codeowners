352bfe726a72c9da92677f672c5bb876ec4e9d63
Nicholas Musolino <n.musolino@gmail.com>
2018-12-02 13:09:07 -0500

    Improve CLI options

--- codeowners/cli.py
+++ codeowners/cli.py
@@ -8,9 +8,12 @@ from codeowners import codeowners, fs_utils
 
 
 @click.command()
-@click.option('--recurse/--no-recurse', is_flag=True, default=True, help='Recursively walk the filesystem')
+@click.version_option()
+@click.option('--only-tracked/--untracked', is_flag=True, default=True,
+              help='Include only files tracked by git in output, or include untracked files.  Default: include only tracked files.')
+@click.option('--recurse/--no-recurse', is_flag=True, default=True, help='Recursively walk the filesystem.  Default: recurves.')
 @click.argument('paths', type=click.Path(), nargs=-1)
-def main(paths, recurse):
+def main(paths, only_tracked, recurse):
     click.echo('Paths: {}'.format(paths))
     if len(paths) == 0:
         paths = ('.',)
